86792582efcd951b57bc8c417448424054f6ff5c
Azhari Rizkita Priyono <azharizkita@outlook.com>
2019-04-01 12:11:34 +0700

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,431 @@
+var profit = [
+    {
+      "tanggal": "2019-04-01",
+      "pendapatan": "920000",
+      "pengeluaran": "260000",
+      "keuntungan": "660000"
+    },
+    {
+      "tanggal": "2019-04-02",
+      "pendapatan": "410000",
+      "pengeluaran": "640000",
+      "keuntungan": "-230000"
+    },
+    {
+      "tanggal": "2019-04-03",
+      "pendapatan": "880000",
+      "pengeluaran": "430000",
+      "keuntungan": "450000"
+    },
+    {
+      "tanggal": "2019-04-04",
+      "pendapatan": "760000",
+      "pengeluaran": "320000",
+      "keuntungan": "440000"
+    },
+    {
+      "tanggal": "2019-04-05",
+      "pendapatan": "540000",
+      "pengeluaran": "250000",
+      "keuntungan": "290000"
+    },
+    {
+      "tanggal": "2019-04-06",
+      "pendapatan": "830000",
+      "pengeluaran": "280000",
+      "keuntungan": "550000"
+    },
+    {
+      "tanggal": "2019-04-07",
+      "pendapatan": "700000",
+      "pengeluaran": "110000",
+      "keuntungan": "590000"
+    },
+    {
+      "tanggal": "2019-04-08",
+      "pendapatan": "860000",
+      "pengeluaran": "160000",
+      "keuntungan": "700000"
+    },
+    {
+      "tanggal": "2019-04-09",
+      "pendapatan": "750000",
+      "pengeluaran": "340000",
+      "keuntungan": "410000"
+    },
+    {
+      "tanggal": "2019-04-10",
+      "pendapatan": "810000",
+      "pengeluaran": "110000",
+      "keuntungan": "700000"
+    },
+    {
+      "tanggal": "2019-04-11",
+      "pendapatan": "800000",
+      "pengeluaran": "190000",
+      "keuntungan": "610000"
+    },
+    {
+      "tanggal": "2019-04-12",
+      "pendapatan": "930000",
+      "pengeluaran": "490000",
+      "keuntungan": "440000"
+    },
+    {
+      "tanggal": "2019-04-13",
+      "pendapatan": "880000",
+      "pengeluaran": "580000",
+      "keuntungan": "300000"
+    },
+    {
+      "tanggal": "2019-04-14",
+      "pendapatan": "840000",
+      "pengeluaran": "670000",
+      "keuntungan": "170000"
+    },
+    {
+      "tanggal": "2019-04-15",
+      "pendapatan": "870000",
+      "pengeluaran": "510000",
+      "keuntungan": "360000"
+    },
+    {
+      "tanggal": "2019-04-16",
+      "pendapatan": "950000",
+      "pengeluaran": "460000",
+      "keuntungan": "490000"
+    },
+    {
+      "tanggal": "2019-04-17",
+      "pendapatan": "400000",
+      "pengeluaran": "100000",
+      "keuntungan": "300000"
+    },
+    {
+      "tanggal": "2019-04-18",
+      "pendapatan": "970000",
+      "pengeluaran": "590000",
+      "keuntungan": "380000"
+    },
+    {
+      "tanggal": "2019-04-19",
+      "pendapatan": "710000",
+      "pengeluaran": "280000",
+      "keuntungan": "430000"
+    },
+    {
+      "tanggal": "2019-04-20",
+      "pendapatan": "460000",
+      "pengeluaran": "160000",
+      "keuntungan": "300000"
+    },
+    {
+      "tanggal": "2019-04-21",
+      "pendapatan": "850000",
+      "pengeluaran": "520000",
+      "keuntungan": "330000"
+    },
+    {
+      "tanggal": "2019-04-22",
+      "pendapatan": "760000",
+      "pengeluaran": "100000",
+      "keuntungan": "660000"
+    },
+    {
+      "tanggal": "2019-04-23",
+      "pendapatan": "950000",
+      "pengeluaran": "600000",
+      "keuntungan": "350000"
+    },
+    {
+      "tanggal": "2019-04-24",
+      "pendapatan": "400000",
+      "pengeluaran": "140000",
+      "keuntungan": "260000"
+    },
+    {
+      "tanggal": "2019-04-25",
+      "pendapatan": "640000",
+      "pengeluaran": "430000",
+      "keuntungan": "210000"
+    },
+    {
+      "tanggal": "2019-04-26",
+      "pendapatan": "570000",
+      "pengeluaran": "650000",
+      "keuntungan": "-80000"
+    },
+    {
+      "tanggal": "2019-04-27",
+      "pendapatan": "430000",
+      "pengeluaran": "540000",
+      "keuntungan": "-110000"
+    },
+    {
+      "tanggal": "2019-04-28",
+      "pendapatan": "780000",
+      "pengeluaran": "150000",
+      "keuntungan": "630000"
+    },
+    {
+      "tanggal": "2019-04-29",
+      "pendapatan": "730000",
+      "pengeluaran": "520000",
+      "keuntungan": "210000"
+    },
+    {
+      "tanggal": "2019-04-30",
+      "pendapatan": "510000",
+      "pengeluaran": "200000",
+      "keuntungan": "310000"
+    },
+    {
+      "tanggal": "2019-05-01",
+      "pendapatan": "820000",
+      "pengeluaran": "340000",
+      "keuntungan": "480000"
+    },
+    {
+      "tanggal": "2019-05-02",
+      "pendapatan": "410000",
+      "pengeluaran": "580000",
+      "keuntungan": "-170000"
+    },
+    {
+      "tanggal": "2019-05-03",
+      "pendapatan": "500000",
+      "pengeluaran": "490000",
+      "keuntungan": "10000"
+    },
+    {
+      "tanggal": "2019-05-04",
+      "pendapatan": "980000",
+      "pengeluaran": "360000",
+      "keuntungan": "620000"
+    },
+    {
+      "tanggal": "2019-05-05",
+      "pendapatan": "570000",
+      "pengeluaran": "210000",
+      "keuntungan": "360000"
+    },
+    {
+      "tanggal": "2019-05-06",
+      "pendapatan": "950000",
+      "pengeluaran": "450000",
+      "keuntungan": "500000"
+    },
+    {
+      "tanggal": "2019-05-07",
+      "pendapatan": "700000",
+      "pengeluaran": "420000",
+      "keuntungan": "280000"
+    },
+    {
+      "tanggal": "2019-05-08",
+      "pendapatan": "620000",
+      "pengeluaran": "670000",
+      "keuntungan": "-50000"
+    },
+    {
+      "tanggal": "2019-05-09",
+      "pendapatan": "540000",
+      "pengeluaran": "410000",
+      "keuntungan": "130000"
+    },
+    {
+      "tanggal": "2019-05-10",
+      "pendapatan": "710000",
+      "pengeluaran": "550000",
+      "keuntungan": "160000"
+    },
+    {
+      "tanggal": "2019-05-11",
+      "pendapatan": "750000",
+      "pengeluaran": "580000",
+      "keuntungan": "170000"
+    },
+    {
+      "tanggal": "2019-05-12",
+      "pendapatan": "470000",
+      "pengeluaran": "500000",
+      "keuntungan": "-30000"
+    },
+    {
+      "tanggal": "2019-05-13",
+      "pendapatan": "520000",
+      "pengeluaran": "290000",
+      "keuntungan": "230000"
+    },
+    {
+      "tanggal": "2019-05-14",
+      "pendapatan": "550000",
+      "pengeluaran": "330000",
+      "keuntungan": "220000"
+    },
+    {
+      "tanggal": "2019-05-15",
+      "pendapatan": "500000",
+      "pengeluaran": "410000",
+      "keuntungan": "90000"
+    },
+    {
+      "tanggal": "2019-05-16",
+      "pendapatan": "720000",
+      "pengeluaran": "510000",
+      "keuntungan": "210000"
+    },
+    {
+      "tanggal": "2019-05-17",
+      "pendapatan": "910000",
+      "pengeluaran": "650000",
+      "keuntungan": "260000"
+    },
+    {
+      "tanggal": "2019-05-19",
+      "pendapatan": "990000",
+      "pengeluaran": "220000",
+      "keuntungan": "770000"
+    },
+    {
+      "tanggal": "2019-05-20",
+      "pendapatan": "720000",
+      "pengeluaran": "520000",
+      "keuntungan": "200000"
+    },
+    {
+      "tanggal": "2019-05-21",
+      "pendapatan": "920000",
+      "pengeluaran": "330000",
+      "keuntungan": "590000"
+    },
+    {
+      "tanggal": "2019-05-23",
+      "pendapatan": "440000",
+      "pengeluaran": "430000",
+      "keuntungan": "10000"
+    },
+    {
+      "tanggal": "2019-05-24",
+      "pendapatan": "480000",
+      "pengeluaran": "160000",
+      "keuntungan": "320000"
+    },
+    {
+      "tanggal": "2019-05-25",
+      "pendapatan": "620000",
+      "pengeluaran": "500000",
+      "keuntungan": "120000"
+    },
+    {
+      "tanggal": "2019-05-26",
+      "pendapatan": "640000",
+      "pengeluaran": "690000",
+      "keuntungan": "-50000"
+    },
+    {
+      "tanggal": "2019-05-27",
+      "pendapatan": "910000",
+      "pengeluaran": "610000",
+      "keuntungan": "300000"
+    },
+    {
+      "tanggal": "2019-05-28",
+      "pendapatan": "590000",
+      "pengeluaran": "580000",
+      "keuntungan": "10000"
+    },
+    {
+      "tanggal": "2019-05-29",
+      "pendapatan": "560000",
+      "pengeluaran": "610000",
+      "keuntungan": "-50000"
+    },
+    {
+      "tanggal": "2019-05-30",
+      "pendapatan": "560000",
+      "pengeluaran": "620000",
+      "keuntungan": "-60000"
+    }
+  ];
+
+
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawVisualization);
+
+function drawVisualization() {
+
+var data = new google.visualization.DataTable();
+data.addColumn('date', 'Tanggal');
+data.addColumn('number', 'Pendapatan (IDR)');
+data.addColumn('number', 'Pengeluaran (IDR)');
+data.addColumn('number', 'Keuntungan (IDR)');
+
+dataPendapatan = []
+profit.forEach(element => {
+    tanggal = element.tanggal.split('-');
+    dataPendapatan.push([new Date(tanggal[0], tanggal[1], tanggal[2]),  parseInt(element.pendapatan), parseInt(element.pengeluaran), parseInt(element.keuntungan)])
+});
+
+var tableNew = $('#activityList').DataTable( {
+    columnDefs: [
+        { type: 'date-uk', targets: 0 }
+    ],
+    columns: [
+        { title: "Tanggal" },
+        { title: "Pendapatan (IDR)" },
+        { title: "Pengeluaran (IDR)" },
+        { title: "Keuntungan (IDR)" }
+    ]
+} );
+
+var dataTable = [];
+var rowId = 0;
+var day = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu']
+var month = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember']
+let currentMonth = 0;
+dataPendapatan.forEach(element => {
+    if (element[0].getMonth() == 5) {
+        data.addRows([
+            [element[0], element[1], element[2], element[3]]
+        ]);
+        if (element[3] <= 0) {
+            element[3] = '<div style="color: red">'+element[3]+'</div>';
+        } else {
+            element[3] = '<div style="color: green">'+element[3]+'</div>'
+        }
+        var syncData = {
+            "Tanggal": day[element[0].getDay()]+', '+element[0].getDate()+'/'+element[0].getMonth()+'/'+element[0].getFullYear(),
+            "pendapatan": '<div style="color: blue">'+element[1]+'</div>',
+            "Pendapatan": '<div style="color: orange">'+element[2]+'</div>',
+            "Keuntungan": element[3]
+        }
+    dataTable[rowId] = Object.values(syncData);
+    rowId++;
+    currentMonth = month[element[0].getMonth()]
+    }
+});
+tableNew.clear().draw();
+tableNew.rows.add(dataTable);
+tableNew.columns.adjust().draw();
+
+var options = {
+    title : 'Rekap Pendapatan Bulan '+currentMonth,
+    height: 900,
+    seriesType: 'bars',
+    series: {
+        1: {
+            color: 'orange'
+        },
+        2: {
+            type: 'line',
+            color: 'red'
+        }
+    }
+};
+
+var chart = new google.visualization.ComboChart(document.getElementById('chart'));
+chart.draw(data, options);
+}
+
+
